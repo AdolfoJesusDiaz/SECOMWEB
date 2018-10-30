2c24694b796fd00fe37873ea8bab9825007cec2f
Adolfo Díaz Capcha <34204089+AdolfoJesusDiaz@users.noreply.github.com>
2018-10-30 15:03:13 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+[remote "origin"]
+url="https://github.com/AdolfoJesusDiaz/SECOMWEB.git"
+[brach "master"]
+remote=origin
+
+[core]
+	repositoryformatversion = 0
+	filemode = false
+	bare = false
+	logallrefupdates = true
+	symlinks = false
+	ignorecase = true
